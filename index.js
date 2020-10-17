4b699fd2a4bc2d88f8febf2bb3fb25768d70fe31
AmanParmar-git <harshparamar84@gmail.com>
2020-10-17 11:25:29 +0530

    second

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { spawn } = require("child_process");
+const os = require("os");
 
-const p = spawn("msiinv.exe", [
-  "-s",
-  "|",
-  'Select-String "SQL Server"',
-  "-Context 0,1",
-]);
+const platform = os.platform();
+let p;
+if (platform === "linux") {
+  p = spawn("apt", ["list", "--installed"]);
+}
 
 p.stdout.pipe(process.stdout);
+process.stdin.pipe(p.stdin);
+
+p.on("close", (c) => process.exit(c));
