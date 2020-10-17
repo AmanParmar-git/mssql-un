3cdc89c55f9b36ae62ba24ab3e5e1d6cef77c756
AmanParmar-git <harshparamar84@gmail.com>
2020-10-17 16:43:31 +0530

    fourth

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const { spawn } = require("child_process");
-const os = require("os");
+const { exec } = require("child_process");
+const fs = require("fs");
 
-const platform = os.platform();
-let p;
-let d = "";
-if (platform === "linux") {
-  p = spawn("apt", ["list", "--installed"]);
-}
-p.stdout.on("data", (data) => {
-  d += data;
-});
-
-p.stdout.on("end", () => {
-  d.replace("]", "]\n");
-  const arr = d.split("\n");
-  for (let a of arr) {
-    if (a.startsWith("python")) {
-      const s = spawn("apt", ["remove", [a]]);
-      s.stdout.pipe(process.stdout);
+exec(
+  "wmic product where \"name like '%sql server%'\" get name,identifyingnumber",
+  (err, data) => {
+    if (err) throw err;
+    let arr = data.trim().split("\n");
+    arr = arr.filter((t) => t.includes("MySQL") === false);
+    let res = [];
+    for (let i = 1; i < arr.length; i++) {
+      const index = arr[i].indexOf("} ");
+      const guid = arr[i].substring(0, index + 1);
+      const name = arr[i].slice(index + 1).trim();
+      res.push({ name: name, guid: guid });
     }
+    let batStr = "";
+    for (let a of res) {
+      batStr +=
+        "rem " + a.name + "\n" + "msiexec /x " + `"${a.guid}" ` + "\n\n";
+    }
+
+    fs.writeFileSync("remove.bat", batStr);
   }
-});
-p.on("close", (c) => {
-  process.exit(c);
-});
+);
