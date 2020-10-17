163b6e3218ea545347796c5fc81f14f726d09040
AmanParmar-git <harshparamar84@gmail.com>
2020-10-17 18:20:47 +0530

    fifth

--- mssqlun.js
+++ mssqlun.js
@@ -7,19 +7,16 @@ exec(
     if (err) throw err;
     let arr = data.trim().split("\n");
     arr = arr.filter((t) => t.includes("MySQL") === false);
-    let res = [];
+    let batStr = "";
+    console.log("Listing....");
     for (let i = 1; i < arr.length; i++) {
       const index = arr[i].indexOf("} ");
       const guid = arr[i].substring(0, index + 1);
       const name = arr[i].slice(index + 1).trim();
-      res.push({ name: name, guid: guid });
+      console.log(name);
+      batStr += "rem " + name + "\n" + "msiexec /x " + `"${guid}" ` + "\n\n";
     }
-    let batStr = "";
-    for (let a of res) {
-      batStr +=
-        "rem " + a.name + "\n" + "msiexec /x " + `"${a.guid}" ` + "\n\n";
-    }
-
     fs.writeFileSync("remove.bat", batStr);
+    console.log("Run remove.bat !!");
   }
 );
---
+++ remove.bat
@@ -0,0 +1,90 @@
+rem Microsoft SQL Server 2019 T-SQL Language Service
+msiexec /x "{31D27B41-A051-49D8-907A-62E0F4A2188C}" 
+
+rem Microsoft OLE DB Driver for SQL Server
+msiexec /x "{74A97B61-DE37-40DF-9E00-B302E5D3C4CE}" 
+
+rem SQL Server 2019 XEvent
+msiexec /x "{228C3DC2-695E-4FC7-87E4-6A9CE905DA9B}" 
+
+rem SQL Server 2019 Connection Info
+msiexec /x "{FD730873-33D1-4D1F-9AE0-E259586F8827}" 
+
+rem SQL Server 2019 DMF
+msiexec /x "{FC8DC283-4A85-467F-8D0E-2FE4606DCCA1}" 
+
+rem Microsoft SQL Server 2019 RsFx Driver
+msiexec /x "{5825CDC4-4E99-4CF9-91FE-DB60C0E2F5EA}" 
+
+rem Microsoft VSS Writer for SQL Server 2019
+msiexec /x "{2C33F4D4-E9A5-4DE1-ACFE-3A13464E6703}" 
+
+rem SQL Server 2019 Connection Info
+msiexec /x "{99B940D5-1A49-4B6C-B26C-6A88B2C061CA}" 
+
+rem SQL Server 2019 Database Engine Shared
+msiexec /x "{DE5B7937-D5B5-4157-BC30-BB87F021CFF0}" 
+
+rem Browser for SQL Server 2019
+msiexec /x "{5E366957-8D78-4BB5-A790-96F97A9766BD}" 
+
+rem Microsoft SQL Server 2012 Native Client
+msiexec /x "{9D93D367-A2CC-4378-BD63-79EF3FE76C78}" 
+
+rem SQL Server 2019 DMF
+msiexec /x "{814D5077-C93F-42E2-B875-717007C186B9}" 
+
+rem SQL Server 2019 Shared Management Objects Extensions
+msiexec /x "{C7E6D4B7-CB10-4239-BA04-D9339B39D0BD}" 
+
+rem SQL Server 2019 SQL Diagnostics
+msiexec /x "{28ED6838-D8E5-454C-A813-12C5EB447CAB}" 
+
+rem Microsoft ODBC Driver 17 for SQL Server
+msiexec /x "{E36FFC78-D25E-4962-872B-9CE0E50E62CD}" 
+
+rem SQL Server Management Studio for Analysis Services
+msiexec /x "{12010719-512E-407E-A83E-18E2CBB867CE}" 
+
+rem SQL Server 2019 Shared Management Objects
+msiexec /x "{A8581199-F913-443B-B058-8E8BF317E71C}" 
+
+rem SQL Server 2019 Common Files
+msiexec /x "{5E4344C9-8B97-4ED9-8760-57E221C240F4}" 
+
+rem SQL Server Management Studio
+msiexec /x "{67FD1BCA-F06F-48F5-86E9-AC4EA982A775}" 
+
+rem SQL Server 2019 Shared Management Objects Extensions
+msiexec /x "{8DDAEBCA-4267-4E16-9FE0-D87F21D36891}" 
+
+rem SQL Server 2019 Batch Parser
+msiexec /x "{D459615B-83B0-408F-8F39-6CC07C277BA6}" 
+
+rem SQL Server 2019 Shared Management Objects
+msiexec /x "{6213D6CB-D258-47A3-B1A0-EE1E5C080DCF}" 
+
+rem SQL Server 2019 Database Engine Services
+msiexec /x "{E3E84B2C-FCF6-469F-9FE7-5E8934DB69AD}" 
+
+rem SQL Server 2019 Database Engine Shared
+msiexec /x "{619F0B6C-C802-422A-B4E5-294E61F68473}" 
+
+rem SQL Server 2019 Common Files
+msiexec /x "{0FB552DD-543E-48E7-A6F4-2F8D82723C6A}" 
+
+rem Microsoft SQL Server 2019 Setup (English)
+msiexec /x "{17DCED0E-5B27-453A-B2B4-E487B869B28A}" 
+
+rem SQL Server 2019 XEvent
+msiexec /x "{2129312E-5204-4F3A-9039-B6D34DBB00FB}" 
+
+rem SQL Server 2019 Database Engine Services
+msiexec /x "{A60B3D8E-5311-4BF1-AF7A-D1AC15F9152E}" 
+
+rem SQL Server Management Studio for Reporting Services
+msiexec /x "{9B3B5F7F-9C68-4F43-9FC0-5551A08618C2}" 
+
+rem SQL Server Management Studio
+msiexec /x "{0955149F-98EC-4F44-8B05-7B0C8C503DCC}" 
+
